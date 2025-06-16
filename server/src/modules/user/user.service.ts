e0a2d7b7603e8fc268e1e126ba6cc00d3bb822b8
Dimitrov Vladislav <Bones4pk@gmail.com>
2025-06-16 23:19:06 +0300

    refactor(user): update create service method to match new schema fields

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -28,10 +28,25 @@ export class UserService {
     static async create(data: {
         email: string;
         password: string;
-        name: string;
         role?: Role;
+        firstName: string;
+        lastName: string;
+        phone: string;
+        acceptedTerms: boolean;
+        acceptedMarketing: boolean;
     }) {
-        return prisma.user.create({ data });
+        return prisma.user.create({
+            data: {
+                email: data.email,
+                password: data.password,
+                role: data.role,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                phone: data.phone,
+                acceptedTerms: data.acceptedTerms,
+                acceptedMarketing: data.acceptedMarketing,
+            },
+        });
     }
 
     /**
