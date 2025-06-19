8b032351d6de60b55b42cc14765584d0d6d014fa
Dimitrov Vladislav <Bones4pk@gmail.com>
2025-06-19 22:31:12 +0300

    refactor(auth.routes): remove redundant function comments

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -3,12 +3,6 @@ import { registerUser } from "./auth.controller";
 
 const router = Router();
 
-/**
- * Authentication routes for user registration.
- * @module auth.routes
- * @requires express
- * @requires auth.controller
- */
 router.post("/register", registerUser);
 
 export default router;
