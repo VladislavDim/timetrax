12874121c76ec2eba73c3b3b7f8086a0c6bd1f28
Dimitrov Vladislav <Bones4pk@gmail.com>
2025-06-24 22:52:46 +0300

    Add Navbar component to main layout
    Imported and rendered the Navbar component above the main content in the App layout to provide site-wide navigation.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import AppRouter from './AppRouter';
+import Navbar from './features/shared/components/Navbar';
 
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
       {/* TODO: Header */}
       <main className="flex-1">
+        {/* Navbar */}
+        <Navbar />
+        {/* Main content */}
         <AppRouter />
       </main>
       {/* TODO: Footer */}
