9884eb66a7bc40e02dadb360b778e58a0a90f323
Dimitrov Vladislav <Bones4pk@gmail.com>
2025-06-24 22:52:34 +0300

    Add HomePage route to AppRouter
    Introduces a route for the HomePage component at the root path ('/'). This allows users to access the HomePage when navigating to the base URL.

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { RegisterFlowPage } from './features/auth/pages/RegisterFlowPage';
+import HomePage from './features/home/pages/HomePage';
 
 export default function AppRouter() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<RegisterFlowPage />} />
     </Routes>
   );
