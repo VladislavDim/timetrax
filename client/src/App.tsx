f0bf0b072ca540bacfddfa07ace8f7c79f2d3a8f
Dimitrov Vladislav <Bones4pk@gmail.com>
2025-06-27 22:57:59 +0300

    Remove Navbar and layout wrappers from App component
    Simplifies the App component by removing the Navbar and layout-related divs, rendering only the AppRouter. This may be in preparation for restructuring the application's layout or moving navigation elsewhere.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,7 @@
 import AppRouter from './AppRouter';
-import Navbar from './features/shared/components/Navbar';
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* TODO: Header */}
-      <main className="flex-1">
-        {/* Navbar */}
-        <Navbar />
-        {/* Main content */}
-        <AppRouter />
-      </main>
-      {/* TODO: Footer */}
-    </div>
+    <AppRouter />
   );
 }
