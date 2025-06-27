86250d5fa0299f9f2d5c4e14090a5a5ed743d387
Dimitrov Vladislav <Bones4pk@gmail.com>
2025-06-27 22:58:28 +0300

    Add RecommendedServices to HomePage
    Imported and rendered the RecommendedServices component in HomePage to display recommended services on the home screen.

--- client/src/features/home/pages/HomePage.tsx
+++ client/src/features/home/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import CallToAction from "../components/CallToAction";
 import Hero from "../components/Hero";
+import RecommendedServices from "../components/RecommendedServices";
 
 export default function HomePage() {
   return (
     <div className="bg-gradient-to-br from-[#fff7e6] via-[#ffd180] to-[#ffb74d]">
       <Hero />
+      <RecommendedServices />
       <CallToAction />
     </div>
   );
